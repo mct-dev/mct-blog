57234bc541b980e7f79c0cad1d3f36c70e227402
Michael Tobias <axis139@gmail.com>
2019-10-05 15:11:34 -0700

    updates deploy script, adds better link styles

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# copy files, then change to submodule dir
-cp public/* mikectobias.com/
-cd mikectobias.com
-
-# Add changes to git.
-git add .
+cd public
+git add --all # Add changes to git.
 
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
 git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
+git push origin master # Push source and build repos.
 cd ..
