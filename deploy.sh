358ae99a528c84bec75cb4da308da2f7dd37c647
Michael Tobias <michael@bitcoinira.com>
2021-03-04 14:42:35 -0800

    - deploy home page changes

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,18 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-cd public
+cp -r -f public/* mikectobias.com/
+
+cd mikectobias.com
 git add --all # Add changes to git.
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="- deployment: `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 
 git commit -m "$msg"
 git push origin master # Push source and build repos.
+
 cd ..
